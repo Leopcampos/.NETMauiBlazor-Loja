cbb725937d9ff7ec5b03eb9b144affb3b6a05dc1
Leo Campos <leopcampos85@gmail.com>
2024-02-01 17:53:14 -0300

    Criação do projeto

---
+++ src/LojaBlazorMaui.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LojaBlazorMaui.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
